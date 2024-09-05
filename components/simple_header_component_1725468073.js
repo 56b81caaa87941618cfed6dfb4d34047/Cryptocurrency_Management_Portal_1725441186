3208927321a8ffb658bd87e807283cda5f071e6c
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-09-05 13:58:18 +0200

    SC Component Generation

--- components/simple_header_component_1725468073.js
+++ components/simple_header_component_1725468073.js
@@ -1,104 +1,118 @@
-<!-- 
-INSTRUCTION: Summary: The header component is a nav bar on top of the page -- it contains the app's logo and name in a big, bold font, to the left. In the middle there are 4 text items each hyperlinked.
--->
 
 <template>
-    <header id="header-section" style="min-height: 110px" class="w-full">
-        <nav id="nav-bar" class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
-            <div id="nav-container" class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-                <a id="logo-link" href="#" class="flex items-center">
-                    <img id="logo-image" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                    <span id="app-name" class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                        CryptoFolio
-                    </span>
-                </a>
-                <div id="menu-toggle-button" class="flex items-center lg:order-2">
-                    <button
-                        id="collapse-button"
-                        data-collapse-toggle="mobile-menu-2"
-                        type="button"
-                        class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="mobile-menu-2"
-                        aria-expanded="false"
-                    >
-                        <span class="sr-only">Open main menu</span>
-                        <svg
-                            id="menu-open-icon"
-                            class="w-6 h-6"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                clip-rule="evenodd"
-                            ></path>
-                        </svg>
-                        <svg
-                            id="menu-close-icon"
-                            class="hidden w-6 h-6"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
-                            ></path>
-                        </svg>
-                    </button>
-                </div>
-                <div id="mobile-menu-2" class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
-                    <ul id="nav-list" class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                        <li id="nav-item-home">
-                            <a
-                                href="#"
-                                class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
-                                aria-current="page"
-                            >
-                                Bitcoin (BTC)
-                            </a>
-                        </li>
-                        <li id="nav-item-1">
-                            <a
-                                href="#"
-                                class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                            >
-                                Ethereum (ETH)
-                            </a>
-                        </li>
-                        <li id="nav-item-2">
-                            <a
-                                href="#"
-                                class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                            >
-                                Ripple (XRP)
-                            </a>
-                        </li>
-                        <li id="nav-item-3">
-                            <a
-                                href="#"
-                                class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                            >
-                                Litecoin (LTC)
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+<div class="container mx-auto p-8 bg-opacity-30 bg-white backdrop-filter backdrop-blur-lg rounded-xl shadow-lg">
+<h1 class="text-4xl font-bold mb-8 text-center text-purple-500">WETH Wrapper</h1>
+<div class="mb-6">
+<p class="text-lg mb-2 text-blue-400">Your ETH Balance: {{ ethBalance }} ETH</p>
+<p class="text-lg mb-4 text-green-400">Your WETH Balance: {{ wethBalance }} WETH</p>
+</div>
+<div class="mb-6">
+<label class="block text-lg mb-2 text-pink-500" for="amount">Amount of ETH to wrap:</label>
+<input class="w-full p-2 rounded-lg bg-opacity-50 bg-white backdrop-filter backdrop-blur-sm border border-purple-300 focus:border-purple-500 focus:ring focus:ring-purple-200" id="amount" min="0" step="0.000000000000000001" type="number" v-model="amount"/>
+</div>
+<button @click="wrapEth" class="w-full py-3 px-6 text-white bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg shadow-md hover:from-purple-600 hover:to-pink-600 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 transition duration-300">
+      Wrap ETH
+    </button>
+<p class="mt-4 text-red-500" v-if="error">{{ error }}</p>
+<p class="mt-4 text-green-500" v-if="success">{{ success }}</p>
+</div>
 </template>
-
 <script>
+import { ethers } from 'ethers'
+
 export default {
-    data() {
-        return {
-            expanded: false,
-            tab: null
-        };
+  name: 'WethWrapper',
+  data() {
+    return {
+      provider: null,
+      signer: null,
+      contract: null,
+      ethBalance: '0',
+      wethBalance: '0',
+      amount: '',
+      error: '',
+      success: '',
+      contractAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      chainId: 1,
+      abi: [
+        {
+          "name": "deposit",
+          "stateMutability": "payable",
+          "inputs": [],
+          "outputs": []
+        },
+        {
+          "name": "balanceOf",
+          "stateMutability": "view",
+          "inputs": [
+            { "type": "address" }
+          ],
+          "outputs": [
+            { "type": "uint256" }
+          ]
+        }
+      ]
     }
-};
+  },
+  methods: {
+    async connectWallet() {
+      if (typeof window.ethereum !== 'undefined') {
+        try {
+          await window.ethereum.request({ method: 'eth_requestAccounts' })
+          this.provider = new ethers.providers.Web3Provider(window.ethereum)
+          this.signer = this.provider.getSigner()
+          this.contract = new ethers.Contract(this.contractAddress, this.abi, this.signer)
+          await this.checkNetwork()
+          await this.updateBalances()
+        } catch (err) {
+          this.error = 'Failed to connect wallet: ' + err.message
+        }
+      } else {
+        this.error = 'Please install MetaMask!'
+      }
+    },
+    async checkNetwork() {
+      const network = await this.provider.getNetwork()
+      if (network.chainId !== this.chainId) {
+        try {
+          await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: '0x' + this.chainId.toString(16) }],
+          })
+        } catch (err) {
+          this.error = 'Please switch to the Ethereum mainnet'
+          throw new Error('Incorrect network')
+        }
+      }
+    },
+    async updateBalances() {
+      const address = await this.signer.getAddress()
+      const ethBalance = await this.provider.getBalance(address)
+      this.ethBalance = ethers.utils.formatEther(ethBalance)
+      const wethBalance = await this.contract.balanceOf(address)
+      this.wethBalance = ethers.utils.formatEther(wethBalance)
+    },
+    async wrapEth() {
+      this.error = ''
+      this.success = ''
+      if (!this.signer) {
+        await this.connectWallet()
+      }
+      try {
+        await this.checkNetwork()
+        const amountWei = ethers.utils.parseEther(this.amount)
+        const tx = await this.contract.deposit({ value: amountWei })
+        await tx.wait()
+        this.success = `Successfully wrapped ${this.amount} ETH to WETH!`
+        await this.updateBalances()
+        this.amount = ''
+      } catch (err) {
+        this.error = 'Failed to wrap ETH: ' + err.message
+      }
+    }
+  },
+  mounted() {
+    this.connectWallet()
+  }
+}
 </script>
